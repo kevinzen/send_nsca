183bdb779ef0075b5db756561035fe691dc3cce2
Jarno Huuskonen <jarno.huuskonen@uef.fi>
2016-03-06 10:30:54 +0200

    connect_and_get_iv: check that recv reads INITIAL_PACKET_LEN bytes.
    (lib)mcrypt encryption support. Supported algorithms:
    3: tripledes, 8: blowfish, 14-16: rijndael-128 - rijndael-256.
    To enable more algorithms figure out key_size / iv_size and add those
    to @@MCRYPT_PARAMS.

--- lib/send_nsca/send_nsca.rb
+++ lib/send_nsca/send_nsca.rb
@@ -1,6 +1,6 @@
 module SendNsca
 
-  STATUS_OK = 0 
+  STATUS_OK = 0
   STATUS_WARNING = 1
   STATUS_CRITICAL = 2
   STATUS_UNKNOWN = 3
@@ -11,20 +11,21 @@ module SendNsca
   class NscaConnection
 
     require 'socket'      # Sockets are in standard library
-    
-    # todo: replace timeout with a better method of handling communication timeouts.
+
+    # todo: replace timeout with a better method of handling communication timeouts. (nonblocking sockets and IO.select ?)
     require 'timeout'
     require 'zlib'
+    require 'mcrypt'
     attr_accessor :nscatimeout
-    
+
     # params for connecting to the nsca/nagios server
     attr_accessor  :nscahost
     attr_accessor  :port
-    
+
     # connection status and error if one found
     attr_reader   :connected
     attr_reader   :error
-    
+
     # read from the nsca/nagios server
     attr_accessor  :iv_and_timestamp
 
@@ -32,16 +33,17 @@ module SendNsca
     attr_accessor  :iv_and_timestamp_str
     attr_accessor  :iv_string
     attr_accessor  :iv
-    attr_accessor  :timestring    
+    attr_accessor  :timestring
     attr_accessor  :timestamp_hex
     attr_accessor  :timestamp
 
     # encryption_mode
-    attr_accessor :encryption_mode
+    attr_accessor  :encryption_mode
+    attr_accessor  :password
 
     # status data to send to nagios
-    # Currently all 4 are required, meaning we only support 'service' passive checkins. 
-    # todo: add host checkins
+    # :hostname, :return_code and :status are required parameters
+    # To send hostcheck omit :service
     attr_accessor  :hostname
     attr_accessor  :service
     attr_accessor  :return_code
@@ -54,7 +56,17 @@ module SendNsca
     attr_accessor  :crc
     PACKET_VERSION = 3
     INITIAL_PACKET_LEN = 132
+    DEFAULT_TIMEOUT = 1
     PACK_STRING = "nxx N a4 n a64 a128 a512xx"
+
+    # MCRYPT parameters (need more algorithms: figure out iv/key sizes)
+    @@MCRYPT_PARAMS = {
+      3  => {:algorithm => 'tripledes',    :key_size => 24, :iv_size => 8},
+      8  => {:algorithm => 'blowfish',     :key_size => 56, :iv_size => 8},
+      14 => {:algorithm => 'rijndael-128', :key_size => 32, :iv_size => 16},
+      15 => {:algorithm => 'rijndael-192', :key_size => 32, :iv_size => 24},
+      16 => {:algorithm => 'rijndael-256', :key_size => 32, :iv_size => 32},
+    }
 
     def initialize(args)
       # connecting to nagios
@@ -66,7 +78,7 @@ module SendNsca
       @status = args[:status]
       @connected = false
       @password = args[:password] || ''
-      @timeout = args[:nscatimeout] || 1
+      @timeout = args[:nscatimeout] || DEFAULT_TIMEOUT
       @encryption_mode = args[:encryption_mode] || SendNsca::XOR
       @debug = args[:debug] || false
     end
@@ -77,7 +89,10 @@ module SendNsca
           @tcp_client = TCPSocket.open(@nscahost, @port)
           @connected = true
           @iv_and_timestamp = @tcp_client.recv(INITIAL_PACKET_LEN)
-        end
+          if @iv_and_timestamp.size != INITIAL_PACKET_LEN then
+            raise "can't read initial packet from server"
+          end
+      end
       rescue
         @connected = false
         @error = "send_ncsa - error connecting to nsca/nagios: #{$!}"
@@ -87,7 +102,7 @@ module SendNsca
       end
       timestamp_for_logging
     end
-    
+
     def convert_timestamp
       # convert timestamp for use in comm to nagios
       @timestring = @iv_and_timestamp[@iv_and_timestamp.length-4,@iv_and_timestamp.length]
@@ -111,13 +126,13 @@ module SendNsca
       convert_timestamp
       convert_iv
     end
-  
+
     def send_nsca
       intialize_nsca_connection
-      @crc = 0 
+      @crc = 0
       nsca_params = [PACKET_VERSION, @crc, @timestring, @return_code, @hostname, @service, @status ]
       string_to_send_without_crc = nsca_params.pack(PACK_STRING)
-      
+
       @crc = Zlib::crc32(string_to_send_without_crc)
       nsca_params = [PACKET_VERSION, @crc, @timestring, @return_code, @hostname, @service, @status ]
       string_to_send_with_crc = nsca_params.pack(PACK_STRING)
@@ -138,7 +153,13 @@ module SendNsca
       if @encryption_mode == SendNsca::NONE
         string_to_send = string_to_send_with_crc
       else
-        string_to_send = SendNsca::NscaConnection.xor(@iv, string_to_send_with_crc, @password)
+        if @encryption_mode == SendNsca::XOR
+          string_to_send = SendNsca::NscaConnection.xor(@iv, string_to_send_with_crc, @password)
+        elsif @@MCRYPT_PARAMS[@encryption_mode]
+          string_to_send = SendNsca::NscaConnection.encrypt(@iv, string_to_send_with_crc, @password, @@MCRYPT_PARAMS[@encryption_mode])
+        else
+          raise "unsupported encryption mode: #{@encryption_mode.tos}"
+        end
         if @debug
           puts "encrypted_string_to_send = #{string_to_send.length}"
           puts "encrypted_string_to_send = #{string_to_send.unpack('H*')}"
@@ -147,10 +168,11 @@ module SendNsca
 
       @tcp_client.send(string_to_send, 0)
       @tcp_client.close
+      @connected = false
     end
 
     def self.xor(iv, str, password='')
-      
+
       str_a = str.unpack("C*")
       iv_a = iv.unpack("C*")
       password_a = password.unpack("C*")
@@ -161,7 +183,21 @@ module SendNsca
         result[i] ^= password_a[i % password_a.size] unless password_a.empty?
       end
 
-      ret_val = result.pack("C*")
+      return result.pack("C*")
+    end
+
+    def self.encrypt(iv, str, password, mcrypt_params)
+      key = ''
+      if password.length < mcrypt_params[:key_size]
+        key = "\0" * mcrypt_params[:key_size]
+        key[0,password.length] = password
+      elsif password.length > mcrypt_params[:key_size]
+        key = password[0,mcrypt_params[:key_size]]
+      else
+        key = password
+      end
+      crypto = Mcrypt.new(mcrypt_params[:algorithm], :cfb, key, iv[0,mcrypt_params[:iv_size]], false)
+      return crypto.encrypt(str)
     end
 
   end
